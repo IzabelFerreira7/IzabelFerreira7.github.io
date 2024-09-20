d87a4bfa30dec321532882d5ad1efb34d39d8aa6
IzabelCorreia <belcorreia07@gmail.com>
2024-09-20 08:41:35 -0300

    20/09

---
+++ Script.js
@@ -0,0 +1,32 @@
+
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.efeito-top', {
+    duration: 2000,
+    distance: '90px'
+})
+// SOBRE
+revelar.reveal('.efeito-sobre', {
+    duration: 2000,
+    distance: '90px',
+    origin:'left'
+})
+
+// PROJETOS 
+revelar.reveal('.efeito-proj1', {
+    duration: 2000,
+    distance: '90px',
+    delay: 400
+})
+
+revelar.reveal('.efeito-proj2', {
+    duration: 2000,
+    distance: '90px',
+    delay: 800
+})
+
+revelar.reveal('.efeito-proj3', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1600
+})
